229d07a8d57af2c4e6f1aa8091fc9e85a7dce847
ahmadshakir21 <ahmad.software.developer@gmail.com>
2022-07-31 16:48:52 +0300

    Commit Commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:group_project/register.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Register());
   }
 }
