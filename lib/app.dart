51f415b8326ee78b2277db5d30cfc96532e64f85
Miran Amanj <miran.am2001@gmail.com>
2022-07-31 17:21:44 +0300

    some error fixed

--- lib/app.dart
+++ lib/app.dart
@@ -1,24 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-<<<<<<< HEAD
-import 'package:group_project/SignIn.dart';
-=======
 import 'package:group_project/register.dart';
->>>>>>> 229d07a8d57af2c4e6f1aa8091fc9e85a7dce847
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignInScreen(),
+      home: Register(),
     );
-=======
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Register());
->>>>>>> 229d07a8d57af2c4e6f1aa8091fc9e85a7dce847
   }
 }
